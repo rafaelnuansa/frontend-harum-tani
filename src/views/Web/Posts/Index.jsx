967c286f77e266dd71d66d2591cedc19f07bfff7
Rafael Nuansa R <iandrewnew@gmail.com>
2024-08-24 18:53:05 +0700

    update

--- src/views/Web/Posts/Index.jsx
+++ src/views/Web/Posts/Index.jsx
@@ -69,7 +69,7 @@ export default function WebPostsIndex() {
         <div classname="row">
           <div className="col-md-12">
             <h5 className="text-uppercase">
-              <i className="fa fa-book"></i> BERITA DESA
+              <i className="fa fa-book"></i> BERITA / POSTS
             </h5>
             <hr />
           </div>
